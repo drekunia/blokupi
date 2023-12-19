5f2d991a0d29b30cb2d150037e6cae4731a2c14b
Andre Kurniawan <drekunia@gmail.com>
2023-12-19 12:27:20 +0700

    generate admin auth

--- lib/blokupi_web/router.ex
+++ lib/blokupi_web/router.ex
@@ -1,6 +1,8 @@
 defmodule BlokupiWeb.Router do
   use BlokupiWeb, :router
 
+  import BlokupiWeb.AdminAuth
+
   import BlokupiWeb.UserAuth
 
   pipeline :browser do
@@ -10,6 +12,7 @@ defmodule BlokupiWeb.Router do
     plug :put_root_layout, html: {BlokupiWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_admin
     plug :fetch_current_user
   end
 
@@ -80,6 +83,44 @@ defmodule BlokupiWeb.Router do
       on_mount: [{BlokupiWeb.UserAuth, :mount_current_user}] do
       live "/users/confirm/:token", UserConfirmationLive, :edit
       live "/users/confirm", UserConfirmationInstructionsLive, :new
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", BlokupiWeb do
+    pipe_through [:browser, :redirect_if_admin_is_authenticated]
+
+    live_session :redirect_if_admin_is_authenticated,
+      on_mount: [{BlokupiWeb.AdminAuth, :redirect_if_admin_is_authenticated}] do
+      live "/admins/register", AdminRegistrationLive, :new
+      live "/admins/log_in", AdminLoginLive, :new
+      live "/admins/reset_password", AdminForgotPasswordLive, :new
+      live "/admins/reset_password/:token", AdminResetPasswordLive, :edit
+    end
+
+    post "/admins/log_in", AdminSessionController, :create
+  end
+
+  scope "/", BlokupiWeb do
+    pipe_through [:browser, :require_authenticated_admin]
+
+    live_session :require_authenticated_admin,
+      on_mount: [{BlokupiWeb.AdminAuth, :ensure_authenticated}] do
+      live "/admins/settings", AdminSettingsLive, :edit
+      live "/admins/settings/confirm_email/:token", AdminSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", BlokupiWeb do
+    pipe_through [:browser]
+
+    delete "/admins/log_out", AdminSessionController, :delete
+
+    live_session :current_admin,
+      on_mount: [{BlokupiWeb.AdminAuth, :mount_current_admin}] do
+      live "/admins/confirm/:token", AdminConfirmationLive, :edit
+      live "/admins/confirm", AdminConfirmationInstructionsLive, :new
     end
   end
 end
